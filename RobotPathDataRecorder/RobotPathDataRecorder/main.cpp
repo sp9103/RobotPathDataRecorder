9bcfdc2b8c18fc92223da9bdfb284be4fb9739af
SungPhill Moon <sp9103@naver.com>
2016-08-11 11:44:58 +0900

    로봇 코드 추가

---
+++ RobotPathDataRecorder/RobotPathDataRecorder/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <afx.h>
+#include <stdlib.h>
+
+#include "ColorBasedTracker.h"
+#include "Robot\RobotArm.h"
+#include "ARMSDK\include\ARMSDK.h"
+
+#ifdef _DEBUG
+#pragma comment(lib, "ARMSDKd.lib")
+#endif
+#ifdef NDEBUG
+#pragma comment(lib, "ARMSDK.lib") 
+#endif
+
+int main(){
+
+	return 0;
+}
